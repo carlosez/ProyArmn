c558ef8b551c904c8ed293c6e1f8011e38cf72e7
Carlos Torres <carlos.eztorres@gmail.com>
2012-06-17 09:17:16 -0600

    Inicializacion del proyecto

---
+++ ControServos/ControServos.ino
@@ -0,0 +1,112 @@
+#include <Servo.h>
+
+
+int EnPosicion[5];
+int IrPosicion[5];
+Servo s1,s2,s3,s4,s5,s6;
+
+int LogServo[20];
+int LogPosiciones[20];
+
+void setup()
+{
+
+s1.attach(3);
+s2.attach(5);
+s3.attach(6);
+s4.attach(9);
+s5.attach(10);
+s6.attach(11);
+
+
+}
+
+void loop()
+{
+
+  int time = millis();
+  if (time > millis() + 60000)
+  {
+  time =0;
+  MoveServo(1,170);
+  }
+  
+  
+  
+
+}
+
+
+
+void UpdateServos()
+{
+  
+for (int i=0; i < 5; i++)
+{
+  if (EnPosicion[i] > IrPosicion[i])
+  {
+    EnPosicion[i]-=1;
+  }
+  
+  if (EnPosicion[i] < IrPosicion[i])
+  {
+    EnPosicion[i] += 1;
+  }
+    
+     MoveServo(i,EnPosicion[i]);
+}
+
+}
+
+
+void MoveServo(int ser, int pos)
+{
+  
+  if (FlagRecording==true)
+  {
+    Resgistrar(ser,pos);
+  }
+  
+  switch(ser)
+  {
+  case 0:
+  s1.write(pos);
+          break;
+  case 1:
+  s2.write(pos);
+          break;
+  case 2:
+  s3.write(pos);
+          break;
+  case 3:
+  s4.write(pos);
+          break;
+  case 4:
+  s5.write(pos);
+          break;       
+   case 5:
+  s6.write(pos); 
+          break;
+          
+  }
+  
+}
+
+
+int VP(int pos)
+{
+  if (pos>180)
+  {pos=180;}
+  if (pos<0)
+  {pos=0;}
+  return pos;
+}
+
+
+
+void Registrar(int ser,int pos)
+{
+    
+
+}
+
