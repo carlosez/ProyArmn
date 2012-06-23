304d5dfd46d62164ea2dd95d17b63caae0186634
Carlos Torres <carlos.eztorres@gmail.com>
2012-06-22 21:46:34 -0600

    Ahora si reconoce comandos y se pueden manipular funciones

--- SerialCommands/SerialCommands.ino
+++ SerialCommands/SerialCommands.ino
@@ -4,7 +4,7 @@ String temp1,temp2; // temporary strings
 char inByte; // Byte input from command prompt
 char carray[6]; // character array for string to int // manipulation
 int a,b,c; // temporary numbers
-enum comando {na,hey,hola,minombre,suma,invalid};
+enum comando {na,hey,hola,minombre,suma,invalid,heylo};
 comando opcion=na;
 
 void setup(){
@@ -24,10 +24,12 @@ if (Serial.available() > 0){
 }// end serial.available
 // Process command when NL/CR are entered:
 if (inByte == 10 || inByte == 13){
-{inByte = 0;
-}
+{
+  inByte = 0;
+
 // Respond to a command:
 if      (command.equalsIgnoreCase("hey")){opcion=hey;}
+else if (command.equalsIgnoreCase("heylo")){opcion=heylo;}
 else if (command.indexOf('+') > 0){opcion=suma;}
 else if (command.startsWith("mynameis")){opcion=minombre;}
 else  {
@@ -40,12 +42,17 @@ else  {
 switch(opcion)
 {
 case hey:
-Serial.println("hello there!");
-
-break;
-
-}
+Serial.println("hello there!");break;
+case heylo:
+Serial.println("Halo, no heylo!!!");break;
 
 
 }
+command="";
+opcion=na;
+
+}
+}// fin de recepcoio
+
+
 }
